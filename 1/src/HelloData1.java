3c147ace4529595268804bbc9733cc22bf6d21b4
DenisIzmestyev <supelmont@gmail.com>
2016-12-04 14:49:20 +0300

    +coments

--- 1/src/HelloData1.java
+++ 1/src/HelloData1.java
@@ -1,7 +1,12 @@
 import java.util.Date;
 public class HelloData1 {
 
-	//
+	/* Первая программа-пример книги.
+	 * Выводит строку и текущее число.
+	 * @autor Брюс Эккель
+	 * @autor www.MindView.net
+	 * @version 4.0
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Привет сегодня: ");
