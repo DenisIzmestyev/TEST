4cecd4173ba40afb0538301f9912631eaaf0913f
DenisIzmestyev <supelmont@gmail.com>
2016-12-04 14:39:07 +0300

    Create

---
+++ 1/src/HelloData1.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+public class HelloData1 {
+
+	//
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Привет сегодня: ");
+		System.out.println( new Date()  );
+		// 
+	}
+
+}
